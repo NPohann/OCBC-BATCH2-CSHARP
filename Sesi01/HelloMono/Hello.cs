e892116d0d48ef785f042eb3aa1eaad55c169c40
NPohann <npohan44@gmail.com>
2021-11-22 14:19:15 +0700

    first commit

---
+++ Sesi01/HelloMono/Hello.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Sesi01
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hai Nama Saya Pohan");
+        }
+    }
+}
---
+++ Sesi01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sesi01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hai Nama Saya Pohan");
+        }
+    }
+}
